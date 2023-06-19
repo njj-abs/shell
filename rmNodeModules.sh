c4ee487b884fc63a3cbdfec2cdce24eba82aeede
njj-abs <njj@abstractit.in>
2023-06-19 17:24:50 +0530

    Remove node modules.

---
+++ rmNodeModules.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+find_directories() {
+    local threshold="$1"
+    
+    find . -iname node_modules |
+    xargs du |
+    awk -v threshold="$threshold" '$1 >= threshold' |
+    sed 's:/node_modules.*:/node_modules:' |
+    awk '{print $2}' |
+    uniq |
+    paste -sd '\n'
+}
+
+confirm_deletion() {
+    echo "$directories"
+    
+    printf "Are you sure you want to delete these directories? (y/n) "
+    read -r answer </dev/tty
+    
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+        return 0
+    else
+        echo "Deletion canceled."
+        return 1
+    fi
+}
+
+delete_directories() {
+    for directory in $directories; do
+        rm -rf "$directory"
+        if [ $? -eq 0 ]; then
+            echo "Deleted: $directory"
+        else
+            echo "Failed to delete: $directory"
+        fi
+    done
+}
+
+main() {
+    threshold="$1"
+    directories=$(find_directories "$threshold")
+
+    if [ -z "$directories" ]; then
+        echo "No directories to delete."
+        exit 0
+    fi
+
+    if confirm_deletion "$directories"; then
+        delete_directories "$directories"
+    fi
+}
+
+main "$@"
+
