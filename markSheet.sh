08a922a6ab77b10f57e7fbbe6a577e5867274041
njj-abs <njj@abstractit.in>
2023-08-19 12:51:45 +0530

    Bug Fixed.

--- markSheet.sh
+++ markSheet.sh
@@ -1,8 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
+cut -d "," -f1,2,3,4 markSheet.csv | tail -n +2 |
 while IFS="," read -r Name Language Maths Science
 do
     echo "Name: $Name"
     echo "Total: $((Language + Maths + Science))"
     echo ""
-done < <(cut -d "," -f1,2,3,4 markSheet.csv | tail -n +2)
+done
