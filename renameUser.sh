214341397a51b0a1df46e936dc68fe83be3ebfd1
njj-abs <njj@abstractit.in>
2023-09-02 12:55:05 +0530

    Take a backup and change the group name.

--- renameUser.sh
+++ renameUser.sh
@@ -8,11 +8,16 @@ set -e
 olduser=$1
 newuser=$2
 
+cp -r /home/$olduser /dump/$olduser
+
 # Rename the user account
 usermod -l $newuser $olduser
 
 # Rename the home directory
 usermod -d /home/$newuser -m $newuser
+
+# Rename the group name
+groupmod -n $newuser $olduser
 
 # Update ownership and permissions
 chown -R $newuser:$newuser /home/$newuser || true
@@ -20,7 +25,5 @@ chown -R $newuser:$newuser /home/$newuser || true
 # Update occurrences in configuration files
 grep -rlI "/home/$newuser/" -e "$olduser" | uniq | sudo xargs -I {} sed -i "s/$olduser/$newuser/g" '{}'
 
-# Rename the home directory
-mv /home/$olduser /home/$newuser
 
 echo "User '$olduser' has been renamed to '$newuser' and home directory renamed."
