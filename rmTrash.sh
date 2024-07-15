6fdcc7a36ead76bfe3a672ab64ba4534d2d1872a
njj-abs <njj@abstractit.in>
2024-07-15 16:27:31 +0530

    Modified the rmTrash.

--- rmTrash.sh
+++ rmTrash.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-users=("seg" "ipr")
+# Check if at least one user name is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 user1 [user2 ...]"
+    exit 1
+fi
 
-for user in "${users[@]}"; do
+# Loop through each user name provided as argument
+for user in "$@"; do
     sudo rm -rf /home/"$user"/.local/share/Trash/files
-    sudo mkdir /home/"$user"/.local/share/Trash/files
-    
+    sudo mkdir -p /home/"$user"/.local/share/Trash/files
 done
